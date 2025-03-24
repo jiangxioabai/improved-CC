d58a30690d0e14a5d21fdbe05b3dbeae310e6342
jxb <jiangt@ios.ac.cn>
2025-03-24 11:40:41 +0800

    Fix: remove submodule and re-add code folder as regular directory

---
+++ code/cca.cpp
@@ -0,0 +1,253 @@
+#include "basis.h"
+#include "cca.h"
+#include "cw.h"
+#include "preprocessor.h"
+
+#include <sys/times.h> //these two h files are for linux
+#include <unistd.h>
+
+
+
+
+
+
+// 子句编号从0开始，变量编号从1开始
+// var_neighbor[i][j]表示变量i的第j个邻居下标；如果是0，则表示数组末尾
+int pick_var_1()
+{
+	int         i,k,c,v;
+	int         best_var;
+	lit*		clause_c;
+	
+	/**Greedy Mode**/
+	/*CCD (configuration changed decreasing) mode, the level with configuation chekcing*/
+	if(goodvar_stack_fill_pointer>0) // 如果存在1-step q-flippable变量，goodvar_stack存的1-step q-flippable变量
+	{
+		best_var = goodvar_stack[0];// 选择第一个变量作为候选
+		// 遍历所有1-step q-flippable变量，选分数最高的变量，相同则选上次翻转最早的变量，这里time_stamp[]初始化应该是0，表示上次反转时间
+		for(i=1; i<goodvar_stack_fill_pointer; ++i)
+		{
+			v=goodvar_stack[i];
+			if(score[v]>score[best_var]) best_var = v;
+			else if(score[v]==score[best_var] && time_stamp[v]<time_stamp[best_var]) best_var = v;
+		}
+		
+		return best_var;
+	}
+	
+	/*SD (significant decreasing) mode, the level with aspiration*/
+	best_var = 0;
+	for(i=0; i<unsatvar_stack_fill_pointer; ++i)// 遍历所有不满足子句中的变量
+	{
+		if(score[unsatvar_stack[i]]>sigscore) 
+		{
+			best_var = unsatvar_stack[i];// 先找到一个满足大于sigscore的变量
+			break;
+		}
+	}
+	// 继续遍历不满足子句中的变量找到分数最大的变量，相同则选择最早翻转的变量（和上面相同）
+	for(++i; i<unsatvar_stack_fill_pointer; ++i)
+	{
+		v=unsatvar_stack[i];
+		if(score[v]>score[best_var]) best_var = v;
+		else if(score[v]==score[best_var] && time_stamp[v]<time_stamp[best_var]) best_var = v;
+	}
+		
+	if(best_var!=0) return best_var;
+	// 如果既没有1-step q-flippable变量，也没有SD变量，则更新子句权重，并随机游走
+	/**Diversification Mode**/
+
+	update_clause_weights();
+	
+	/*focused random walk*/
+
+	c = unsat_stack[rand()%unsat_stack_fill_pointer];//随机选择一个不满足子句
+	clause_c = clause_lit[c];
+	best_var = clause_c[0].var_num;//将子句中的第一个变量作为候选
+	// 同样倾向于选择该随机子句中分数最高的变量，相同则选上次翻转最早的变量
+	for(k=1; k<clause_lit_count[c]; ++k)
+	{
+		v=clause_c[k].var_num;
+		//if(time_stamp[v]<time_stamp[best_var]) best_var = v;
+		if(score[v]>score[best_var]) best_var = v;
+		else if(score[v]==score[best_var]&&time_stamp[v]<time_stamp[best_var]) best_var = v;
+	}
+	
+	return best_var;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//pick a var to be flip // 从候选中选择一个变量进行翻转
+int pick_var()
+{
+	int         i,k,c,v;
+	int         best_var;
+	lit*		clause_c;
+	
+	/**Greedy Mode**/
+	/*CCD (configuration changed decreasing) mode, the level with configuation chekcing*/
+	if(goodvar_stack_fill_pointer>0) // 如果存在1-step q-flippable变量，goodvar_stack存的1-step q-flippable变量
+	{
+		best_var = goodvar_stack[0];// 选择第一个变量作为候选
+		// 遍历所有1-step q-flippable变量，选分数最高的变量，相同则选上次翻转最早的变量，这里time_stamp[]初始化应该是0，表示上次反转时间
+		for(i=1; i<goodvar_stack_fill_pointer; ++i)
+		{
+			v=goodvar_stack[i];
+			if(score[v]>score[best_var]) best_var = v;
+			else if(score[v]==score[best_var] && time_stamp[v]<time_stamp[best_var]) best_var = v;
+		}
+		
+		return best_var;
+	}
+	
+	/*SD (significant decreasing) mode, the level with aspiration*/
+	best_var = 0;
+	for(i=0; i<unsatvar_stack_fill_pointer; ++i)// 遍历所有不满足子句中的变量
+	{
+		if(score[unsatvar_stack[i]]>sigscore) 
+		{
+			best_var = unsatvar_stack[i];// 先找到一个满足大于sigscore的变量
+			break;
+		}
+	}
+	// 继续遍历不满足子句中的变量找到分数最大的变量，相同则选择最早翻转的变量（和上面相同）
+	for(++i; i<unsatvar_stack_fill_pointer; ++i)
+	{
+		v=unsatvar_stack[i];
+		if(score[v]>score[best_var]) best_var = v;
+		else if(score[v]==score[best_var] && time_stamp[v]<time_stamp[best_var]) best_var = v;
+	}
+		
+	if(best_var!=0) return best_var;
+	// 如果既没有1-step q-flippable变量，也没有SD变量，则更新子句权重，并随机游走
+	/**Diversification Mode**/
+
+	update_clause_weights();
+	
+	/*focused random walk*/
+
+	c = unsat_stack[rand()%unsat_stack_fill_pointer];//随机选择一个不满足子句
+	clause_c = clause_lit[c];
+	best_var = clause_c[0].var_num;//将子句中的第一个变量作为候选
+	// 同样倾向于选择该随机子句中分数最高的变量，相同则选上次翻转最早的变量
+	for(k=1; k<clause_lit_count[c]; ++k)
+	{
+		v=clause_c[k].var_num;
+		//if(time_stamp[v]<time_stamp[best_var]) best_var = v;
+		if(score[v]>score[best_var]) best_var = v;
+		else if(score[v]==score[best_var]&&time_stamp[v]<time_stamp[best_var]) best_var = v;
+	}
+	
+	return best_var;
+}
+
+
+//set functions in the algorithm 设置子句权重
+void settings()
+{
+	set_clause_weighting();
+}
+
+void local_search(int max_flips)
+{
+	int flipvar;
+     
+	for (step = 0; step<max_flips; step++)
+	{
+		//find a solution
+		if(unsat_stack_fill_pointer==0) return;
+
+		flipvar = pick_var();
+
+		flip(flipvar);
+
+		time_stamp[flipvar] = step;
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	int     seed,i;
+	int		satisfy_flag=0;
+	struct 	tms start, stop;
+    
+    cout<<"c This is CCAnr [Version: 2013.4.18] [Author: Shaowei Cai]."<<endl;	
+	
+	times(&start);
+
+	if (build_instance(argv[1])==0)// 构建实例
+	{
+		cout<<"Invalid filename: "<< argv[1]<<endl;
+		return -1;
+	}
+
+    sscanf(argv[2],"%d",&seed);
+    
+    srand(seed);
+    
+    if(unitclause_queue_end_pointer>0) preprocess();// 进行预处理，消掉单变量子句
+    
+    build_neighbor_relation();// 构建变量邻居关系
+    
+	cout<<"c Instance: Number of variables = "<<num_vars<<endl;
+	cout<<"c Instance: Number of clauses = "<<num_clauses<<endl;
+	cout<<"c Instance: Ratio = "<<ratio<<endl;
+	cout<<"c Instance: Formula length = "<<formula_len<<endl;
+	cout<<"c Instance: Avg (Min,Max) clause length = "<<avg_clause_len<<" ("<<min_clause_len<<","<<max_clause_len<<")"<<endl;
+	cout<<"c Algorithmic: Random seed = "<<seed<<endl;
+    //多次局部搜索
+	for (tries = 0; tries <= max_tries; tries++)
+	{
+		 settings(); //初始化设置子句权重
+		 
+		 init();  //初始化
+	 
+		 local_search(max_flips);//局部搜索
+
+		 if (unsat_stack_fill_pointer==0) 
+		 {
+		 	if(verify_sol()==1) {satisfy_flag = 1; break;}
+		    else cout<<"c Sorry, something is wrong."<<endl;/////
+		 }
+	}
+
+	times(&stop);
+	double comp_time = double(stop.tms_utime - start.tms_utime +stop.tms_stime - start.tms_stime) / sysconf(_SC_CLK_TCK);
+
+    if(satisfy_flag==1)
+    {
+    	cout<<"s SATISFIABLE"<<endl;
+		print_solution();
+    }
+    else  cout<<"s UNKNOWN"<<endl;
+    
+    cout<<"c solveSteps = "<<tries<<" tries + "<<step<<" steps (each try has "<<max_flips<<" steps)."<<endl;
+    cout<<"c solveTime = "<<comp_time<<endl;
+	 
+    free_memory();
+
+    return 0;
+}
---
+++ code/cca.h
@@ -0,0 +1,632 @@
+/************************************=== CCAnr ===***************************************          
+ ** CCAnr is a local search solver for the Boolean Satisfiability (SAT) problem,
+ ** which is especially designed for non-random instances.
+ ** CCAnr is designed and implemented by Shaowei Cai (email: shaoweicai.cs@gmail.com),                                        
+ *****************************************************************************************/
+
+/*****************************=== Develpment history ===*************************************
+** 2011.5
+** SWCC (Smoothed Weighting and Configuration Checking) by Shaowei Cai
+** New Idea: Configuration Checking (CC)
+** A variable is configuration changed, if since its last flip, at least one of its 
+** neighboring var has been flipped. 
+** In the greedy mode, Swcc picks the best Configuration Changed Decreasing  var to flip.
+** In the random mode, it updates weights, and flips the oldest var in a random unsat clause.
+ 
+** 2011.9 
+** SWCCA (Smoothed Weighting and Configuration Checking with Aspiration) by Shaowei Cai                 
+** New Idea: CC with Aspiration (CCA)
+** Modification: in greedy mode, it first prefers to flip the best CCD var. If there is 
+** no CCD variable, then flip the best significant decreasing var, i.e., with a great 
+** positive score (in Swcca, bigger than averaged clause weight), if there exsit such vars.                  
+ 
+** 2013.4	
+** CCAnr (CCA for non-random SAT)
+** Modifications: Generalize the smoothig fomula as w(ci)=w(ci)*p+ave_w*q; pick the greediest
+** variable in the diversification mode.
+************************************************************************************************/
+
+
+#ifndef _CCA_H_
+#define _CCA_H_
+
+#include "basis.h"
+
+// -----------------------------------------------------------------------new
+#include <iostream>
+#include <unordered_map>
+#include <deque>
+#include <vector>
+#include <utility>
+// 全局变量var_change[i]表示变量 i 的邻域内最近两次改变的变量队列，长度为2（只记录近两次的）
+std::vector<std::deque<int>> var_change;
+// 需要每次遍历flipvar及其邻居和二次邻居的var_change，用于判断受到影响的是否为unqualified_pairs
+// 定义全局变量 neighbor_pairs 和 unqualified_pairs
+std::set<std::pair<int, int>> neighbor_pairs;
+std::set<std::pair<int, int>> unqualified_pairs;
+// 定义标准unqualified_pairs对，变量编号更小的插入到前面
+std::pair<int, int> normalizePair(int a, int b) {
+    return {std::min(a, b), std::max(a, b)};
+}
+// 新增unqualified_pairs
+void addPair(int a, int b) {
+    auto normalizedPair = normalizePair(a, b);
+	unqualified_pairs.insert(normalizedPair);
+}
+// 移除unqualified_pairs
+void removePair(int a, int b) {
+    auto normalizedPair = normalizePair(a, b);
+	unqualified_pairs.erase(normalizedPair);
+}
+
+
+
+void init_1()
+{
+	int 		v,c;
+	int			i,j;
+	int			clause;
+	// 初始化var_change，长度为 count，每个队列为空
+	var_change = std::vector<std::deque<int>>(num_vars); 
+	// 初始化qualified_pair
+
+
+
+	//Initialize edge weights 初始化子句权重为1
+	for (c = 0; c<num_clauses; c++)
+		clause_weight[c] = 1;
+
+	//init unsat_stack
+	// 初始化不满足子句栈和不满足变量栈
+	unsat_stack_fill_pointer = 0;
+	unsatvar_stack_fill_pointer = 0;
+
+	//init solution
+	for (v = 1; v <= num_vars; v++) {
+        
+        if(fix[v]==0){
+            if(rand()%2==1) cur_soln[v] = 1;
+            else cur_soln[v] = 0;
+
+			time_stamp[v] = 0;
+			conf_change[v] = 1;
+			unsat_app_count[v] = 0;
+		
+			//pscore[v] = 0;
+		}
+		
+	}
+
+	/* figure out sat_count, and init unsat_stack */
+	for (c=0; c<num_clauses; ++c) 
+	{
+		if(clause_delete[c]==1) continue;
+		
+		sat_count[c] = 0;
+		
+		for(j=0; j<clause_lit_count[c]; ++j)
+		{
+			if (cur_soln[clause_lit[c][j].var_num] == clause_lit[c][j].sense)
+			{
+				sat_count[c]++;
+				sat_var[c] = clause_lit[c][j].var_num;	
+			}
+		}
+
+		if (sat_count[c] == 0) 
+			unsat(c);
+	}
+
+	// figure out var score 可以作为一个函数
+	int lit_count;
+	for (v=1; v<=num_vars; v++) 
+	{
+		// 如果变量被固定，则将其得分设置为一个极小值
+		if(fix[v]==1) 
+		{
+			score[v] = -100000;
+			continue;
+		}
+		
+		score[v] = 0;
+		// 获取变量所在的所有文字数量
+		lit_count = var_lit_count[v];
+		// 遍历变量的所有文字，计算得分
+		for(i=0; i<lit_count; ++i)
+		{	// 获取该文字所在子句的编号
+			c = var_lit[v][i].clause_num;
+			if (sat_count[c]==0) score[v]++; // 子句不满足，则flip该变量后，子句满足，因此得分+1
+			else if (sat_count[c]==1 && var_lit[v][i].sense==cur_soln[v]) score[v]--;// 子句仅由当前变量满足，则得分减1
+		}
+	}
+	
+	/*
+	int flag;
+	//compute pscore and record sat_var and sat_var2 for 2sat clauses
+	for (c=0; c<num_clauses; ++c) 
+	{
+		if(clause_delete[c]==1) continue;
+		
+		if (sat_count[c]==1)
+		{
+			for(j=0;j<clause_lit_count[c];++j)
+			{
+				v=clause_lit[c][j].var_num;
+				if(v!=sat_var[c])pscore[v]++;
+			}
+		}
+		else if(sat_count[c]==2)
+		{
+			flag=0;
+			for(j=0;j<clause_lit_count[c];++j)
+			{
+				v=clause_lit[c][j].var_num;
+				if(clause_lit[c][j].sense == cur_soln[v])
+				{
+					pscore[v]--;
+					if(flag==0){sat_var[c] = v; flag=1;}
+					else	{sat_var2[c] = v; break;}
+				}
+			}
+		
+		}
+	}
+	*/
+	
+		
+	//init goodvars stack
+	goodvar_stack_fill_pointer = 0;
+	for (v=1; v<=num_vars; v++) 
+	{
+		if(fix[v]==1)  continue;
+		// 如果变量得分大于 0，则将其加入 goodvar_stack
+		if(score[v]>0)// && conf_change[v]==1)
+		{
+			already_in_goodvar_stack[v] = 1;
+			push(v,goodvar_stack);
+			
+		}// 否则标记为未在 goodvar_stack 中
+		else already_in_goodvar_stack[v] = 0;
+	}
+	
+	//setting for the virtual var 0 时戳初始化为0
+	time_stamp[0]=0;
+	//pscore[0]=0;
+}
+void flip_1(int flipvar)
+{
+	//选择翻转变量后应该更新flipvar及其邻居的var_change
+	//var_change用于判断其和邻居是否为qualifie pair
+	//below
+    //更新flipvar自己的var_change
+	int i,j;
+	if (var_change[flipvar].begin()+1 != var_change[flipvar].end()) 
+	{
+		// 如果历史记录已经有两事件，则移除最早的一个
+		if (var_change[flipvar].size() == 2) {
+			var_change[flipvar].pop_front();
+		}
+		var_change[flipvar].push_back(flipvar);
+		} 
+	else 
+	{
+		// 否则，创建新的历史记录
+		var_change[flipvar] = std::deque<int>{flipvar};
+	}
+	//更新flipvar邻居的var_change
+	for(i = 0; var_neighbor[flipvar][i] != 0; i++)
+	{
+		// 获取邻居编号
+		j = var_neighbor[flipvar][i];
+		if (var_change[j].begin()+1 != var_change[j].end()) 
+		{
+			// 如果历史记录已经有两事件，则移除最早的一个
+			if (var_change[j].size() == 2) {
+				var_change[j].pop_front();
+			}
+			var_change[j].push_back(flipvar);
+			} 
+		else 
+		{
+			// 否则，创建新的历史记录
+			var_change[j] = std::deque<int>{flipvar};
+		}
+	}
+	//above 
+
+
+	cur_soln[flipvar] = 1 - cur_soln[flipvar]; // 翻转 flipvar 的值
+	
+	int v,c;
+
+	lit* clause_c;
+	// 在分数没更新时，保存flipvar的原始得分
+	int org_flipvar_score = score[flipvar];
+	
+	//update related clauses and neighbor vars 遍历flipvar所在的子句，q是flipvar的所有文字，c是文字对应的子句编号
+	for(lit *q = var_lit[flipvar]; (c=q->clause_num)>=0; q++)
+	{
+		clause_c = clause_lit[c];// 获取当前子句的文字列表
+		// 如果翻转后 flipvar 的当前值==子句中的文字真值
+		// 如，q->sense=0时，变量1->0，则文字假变真；q->sense=1时，变量0->1，则文字假变真。else则子句满足数-1
+		if(cur_soln[flipvar] == q->sense)
+		{
+			++sat_count[c];
+			
+			if (sat_count[c] == 2) //sat_count from 1 to 2
+				// 增加满足子句的另一个变量的得分
+				//∵之前翻转另一变量,子句从满足变成不满足,因此子句对另一变量的分数贡献为-clause_weight[c]
+				// 这儿要修改成不加权和加权的两种
+				score[sat_var[c]] += clause_weight[c];
+			else if (sat_count[c] == 1) // sat_count from 0 to 1
+			{
+				sat_var[c] = flipvar;//record the only true lit's var
+				// flipvar翻转后子句才满足
+				// ∵子句其他变量翻转后sat_count由增加变不增加，∴得分减少
+				// ∵flipvar再翻转后sat_count-1，，∴得分也减少
+				for(lit* p=clause_c; (v=p->var_num)!=0; p++) score[v] -= clause_weight[c];
+                // 将子句标记为满足，并更新相关变量
+				sat(c);
+			}
+		}
+		// 如果翻转后 flipvar 的当前值！=子句中的文字真值
+		// 如，q->sense=0时，变量0->1，则文字真变假；q->sense=1时，变量1->0，则文字真变假。else则子句满足数-1
+		else // cur_soln[flipvar] != cur_lit.sense
+		{
+			--sat_count[c];
+			if (sat_count[c] == 1) //sat_count from 2 to 1
+			{
+				for(lit* p=clause_c; (v=p->var_num)!=0; p++) 
+				{
+					// q->sense=0时,cur_soln[v]=0,则当前文字为真，翻转v，sat-1；
+					// q->sense=1时,cur_soln[v]=1,则当前文字为真，翻转v，sat-1；
+					if(p->sense == cur_soln[v])
+					{
+						
+						score[v] -= clause_weight[c];
+						sat_var[c] = v;// 目前唯一满足子句c的变量是v
+						break;
+					}
+				}
+			}
+			else if (sat_count[c] == 0) //sat_count from 1 to 0
+			{
+				// 此时子句c不满足，任意翻转c包含的变量均可使其满足，得分+
+				for(lit* p=clause_c; (v=p->var_num)!=0; p++) score[v] += clause_weight[c];
+				// 将子句标记为不满足，并更新相关变量
+				unsat(c);
+			}//end else if
+			
+		}//end else
+	}
+	// flipvar翻转后，分数为翻转前的相反数，邻居分数已经在上面更新过了
+	score[flipvar] = -org_flipvar_score;
+	
+	/*update CCD */
+	int index;
+	// 因为flipvar刚翻转过，conf_change设置为unflippable
+	conf_change[flipvar] = 0;
+	// flipvar翻转后，更新goodvar_stack中元素，选择1-step q-flippable变量
+	// 条件1：score>0；条件2：conf_change=1
+	//remove the vars no longer goodvar in goodvar stack 
+	for(index=goodvar_stack_fill_pointer-1; index>=0; index--)
+	{
+		v = goodvar_stack[index];
+		// 分数不满足移除，这里会把 flipvar 移除，因为其分数已被更新为负
+		if(score[v]<=0)
+		{
+			goodvar_stack[index] = pop(goodvar_stack);
+			already_in_goodvar_stack[v] = 0;
+		}	
+	}
+
+	//update all flipvar's neighbor's conf_change to be 1, add goodvar
+	// 唯一使用了邻居关系的地方
+	int* p;
+	for(p=var_neighbor[flipvar]; (v=*p)!=0; p++)
+	{
+		conf_change[v] = 1;
+		// 分数大于0，且还未在goodvar_stack，则入栈
+		if(score[v]>0 && already_in_goodvar_stack[v] ==0)
+		{
+			push(v,goodvar_stack);
+			already_in_goodvar_stack[v] = 1;
+		}
+	}
+}
+
+// ------------------------------------------------------------------------new
+
+
+
+
+
+// 用vector来管理，代码更简洁易懂
+#define pop(stack) stack[--stack ## _fill_pointer] //pop则_fill_pointer--
+#define push(item, stack) stack[stack ## _fill_pointer++] = item //push则_fill_pointer++
+
+
+// 将子句标记为不满足，并更新相关变量
+inline void unsat(int clause) //这里clause应该表示子句在整个公式F中的下标（从0开始）
+{   //unsat_stack_fill_pointer指当前新入stack的元素下标，也表示当前stack元素数量
+	index_in_unsat_stack[clause] = unsat_stack_fill_pointer; // 记录子句在不满足栈中的位置
+	push(clause,unsat_stack); // 入栈
+	
+	//update appreance count of each var in unsat clause and update stack of vars in unsat clauses
+	int v;
+	//p是一个lit结构体向量，每个lit包含 子句下标（0开始） 变量下标（1开始） 从句真值
+	//clause_lit[i][j]表示第i个子句的第j个lit，
+	//这里p初始化指向为clause_lit的第clause个子句的第一个lit的指针，子句包含了很多lit结构体
+	//v表示p所指向的lit结构体的变量数这一成员，子句末尾变量一般用0作为标记
+	for(lit* p=clause_lit[clause]; (v=p->var_num)!=0; p++)
+	{	
+		unsat_app_count[v]++;// 增加变量（下标为v）在不满足子句中的出现次数
+		if(unsat_app_count[v]==1)//首次出现，则压入不满足变量栈，并记录位置
+		{
+			index_in_unsatvar_stack[v] = unsatvar_stack_fill_pointer;
+			push(v,unsatvar_stack);	// 将变量压入不满足变量栈
+		}
+	}
+}
+
+// 将子句标记为满足，并更新相关变量
+inline void sat(int clause)
+{
+	int index,last_unsat_clause;
+
+	//since the clause is satisfied, its position can be reused to store the last_unsat_clause
+	//交换函数输入clause和unsat_stack最后一个子句位置，并将clause出栈
+	last_unsat_clause = pop(unsat_stack);// 从不满足栈中移除最新一条不满足的子句
+	index = index_in_unsat_stack[clause];// 获得clause在不满足栈中的index
+	unsat_stack[index] = last_unsat_clause;// 将last_unsat_clause插入到该index中
+	index_in_unsat_stack[last_unsat_clause] = index;// 更新last_unsat_clause的index
+	
+	//update appreance count of each var in unsat clause and update stack of vars in unsat clauses
+	int v,last_unsat_var;
+	for(lit* p=clause_lit[clause]; (v=p->var_num)!=0; p++)
+	{	
+		unsat_app_count[v]--;// 减少变量（下标为v）在不满足子句中的出现次数
+		if(unsat_app_count[v]==0)// 如果次数减少为0，则将其出栈，同样是交换v和unsat_stack最后一个变量位置，并将v出栈
+		{
+			last_unsat_var = pop(unsatvar_stack);
+			index = index_in_unsatvar_stack[v];
+			unsatvar_stack[index] = last_unsat_var;
+			index_in_unsatvar_stack[last_unsat_var] = index;
+		}
+	}
+}
+
+//initiation of the algorithm
+void init()
+{
+	int 		v,c;
+	int			i,j;
+	int			clause;
+	
+	//Initialize edge weights 初始化子句权重为1
+	for (c = 0; c<num_clauses; c++)
+		clause_weight[c] = 1;
+
+	//init unsat_stack
+	// 初始化不满足子句栈和不满足变量栈
+	unsat_stack_fill_pointer = 0;
+	unsatvar_stack_fill_pointer = 0;
+
+	//init solution
+	for (v = 1; v <= num_vars; v++) {
+        
+        if(fix[v]==0){
+            if(rand()%2==1) cur_soln[v] = 1;
+            else cur_soln[v] = 0;
+
+			time_stamp[v] = 0;
+			conf_change[v] = 1;
+			unsat_app_count[v] = 0;
+		
+			//pscore[v] = 0;
+		}
+		
+	}
+
+	/* figure out sat_count, and init unsat_stack */
+	for (c=0; c<num_clauses; ++c) 
+	{
+		if(clause_delete[c]==1) continue;
+		
+		sat_count[c] = 0;
+		
+		for(j=0; j<clause_lit_count[c]; ++j)
+		{
+			if (cur_soln[clause_lit[c][j].var_num] == clause_lit[c][j].sense)
+			{
+				sat_count[c]++;
+				sat_var[c] = clause_lit[c][j].var_num;	
+			}
+		}
+
+		if (sat_count[c] == 0) 
+			unsat(c);
+	}
+
+	// figure out var score 可以作为一个函数
+	int lit_count;
+	for (v=1; v<=num_vars; v++) 
+	{
+		// 如果变量被固定，则将其得分设置为一个极小值
+		if(fix[v]==1) 
+		{
+			score[v] = -100000;
+			continue;
+		}
+		
+		score[v] = 0;
+		// 获取变量所在的所有文字数量
+		lit_count = var_lit_count[v];
+		// 遍历变量的所有文字，计算得分
+		for(i=0; i<lit_count; ++i)
+		{	// 获取该文字所在子句的编号
+			c = var_lit[v][i].clause_num;
+			if (sat_count[c]==0) score[v]++; // 子句不满足，则flip该变量后，子句满足，因此得分+1
+			else if (sat_count[c]==1 && var_lit[v][i].sense==cur_soln[v]) score[v]--;// 子句仅由当前变量满足，则得分减1
+		}
+	}
+	
+	/*
+	int flag;
+	//compute pscore and record sat_var and sat_var2 for 2sat clauses
+	for (c=0; c<num_clauses; ++c) 
+	{
+		if(clause_delete[c]==1) continue;
+		
+		if (sat_count[c]==1)
+		{
+			for(j=0;j<clause_lit_count[c];++j)
+			{
+				v=clause_lit[c][j].var_num;
+				if(v!=sat_var[c])pscore[v]++;
+			}
+		}
+		else if(sat_count[c]==2)
+		{
+			flag=0;
+			for(j=0;j<clause_lit_count[c];++j)
+			{
+				v=clause_lit[c][j].var_num;
+				if(clause_lit[c][j].sense == cur_soln[v])
+				{
+					pscore[v]--;
+					if(flag==0){sat_var[c] = v; flag=1;}
+					else	{sat_var2[c] = v; break;}
+				}
+			}
+		
+		}
+	}
+	*/
+	
+		
+	//init goodvars stack
+	goodvar_stack_fill_pointer = 0;
+	for (v=1; v<=num_vars; v++) 
+	{
+		if(fix[v]==1)  continue;
+		// 如果变量得分大于 0，则将其加入 goodvar_stack
+		if(score[v]>0)// && conf_change[v]==1)
+		{
+			already_in_goodvar_stack[v] = 1;
+			push(v,goodvar_stack);
+			
+		}// 否则标记为未在 goodvar_stack 中
+		else already_in_goodvar_stack[v] = 0;
+	}
+	
+	//setting for the virtual var 0 时戳初始化为0
+	time_stamp[0]=0;
+	//pscore[0]=0;
+}
+
+void flip(int flipvar)
+{
+	cur_soln[flipvar] = 1 - cur_soln[flipvar]; // 翻转 flipvar 的值
+	
+	// int i,j; 没用到
+	int v,c;
+
+	lit* clause_c;
+	// 在分数没更新时，保存flipvar的原始得分
+	int org_flipvar_score = score[flipvar];
+	
+	//update related clauses and neighbor vars 遍历flipvar所在的子句，q是flipvar的所有文字，c是文字对应的子句编号
+	for(lit *q = var_lit[flipvar]; (c=q->clause_num)>=0; q++)
+	{
+		clause_c = clause_lit[c];// 获取当前子句的文字列表
+		// 如果翻转后 flipvar 的当前值==子句中的文字真值
+		// 如，q->sense=0时，变量1->0，则文字假变真；q->sense=1时，变量0->1，则文字假变真。else则子句满足数-1
+		if(cur_soln[flipvar] == q->sense)
+		{
+			++sat_count[c];
+			
+			if (sat_count[c] == 2) //sat_count from 1 to 2
+				// 增加满足子句的另一个变量的得分
+				//∵之前翻转另一变量,子句从满足变成不满足,因此子句对另一变量的分数贡献为-clause_weight[c]
+				// 这儿要修改成不加权和加权的两种
+				score[sat_var[c]] += clause_weight[c];
+			else if (sat_count[c] == 1) // sat_count from 0 to 1
+			{
+				sat_var[c] = flipvar;//record the only true lit's var
+				// flipvar翻转后子句才满足
+				// ∵子句其他变量翻转后sat_count由增加变不增加，∴得分减少
+				// ∵flipvar再翻转后sat_count-1，，∴得分也减少
+				for(lit* p=clause_c; (v=p->var_num)!=0; p++) score[v] -= clause_weight[c];
+                // 将子句标记为满足，并更新相关变量
+				sat(c);
+			}
+		}
+		// 如果翻转后 flipvar 的当前值！=子句中的文字真值
+		// 如，q->sense=0时，变量0->1，则文字真变假；q->sense=1时，变量1->0，则文字真变假。else则子句满足数-1
+		else // cur_soln[flipvar] != cur_lit.sense
+		{
+			--sat_count[c];
+			if (sat_count[c] == 1) //sat_count from 2 to 1
+			{
+				for(lit* p=clause_c; (v=p->var_num)!=0; p++) 
+				{
+					// q->sense=0时,cur_soln[v]=0,则当前文字为真，翻转v，sat-1；
+					// q->sense=1时,cur_soln[v]=1,则当前文字为真，翻转v，sat-1；
+					if(p->sense == cur_soln[v])
+					{
+						
+						score[v] -= clause_weight[c];
+						sat_var[c] = v;// 目前唯一满足子句c的变量是v
+						break;
+					}
+				}
+			}
+			else if (sat_count[c] == 0) //sat_count from 1 to 0
+			{
+				// 此时子句c不满足，任意翻转c包含的变量均可使其满足，得分+
+				for(lit* p=clause_c; (v=p->var_num)!=0; p++) score[v] += clause_weight[c];
+				// 将子句标记为不满足，并更新相关变量
+				unsat(c);
+			}//end else if
+			
+		}//end else
+	}
+	// flipvar翻转后，分数为翻转前的相反数，邻居分数已经在上面更新过了
+	score[flipvar] = -org_flipvar_score;
+	
+	/*update CCD */
+	int index;
+	// 因为flipvar刚翻转过，conf_change设置为unflippable
+	conf_change[flipvar] = 0;
+	// flipvar翻转后，更新goodvar_stack中元素，选择1-step q-flippable变量
+	// 条件1：score>0；条件2：conf_change=1
+	//remove the vars no longer goodvar in goodvar stack 
+	for(index=goodvar_stack_fill_pointer-1; index>=0; index--)
+	{
+		v = goodvar_stack[index];
+		// 分数不满足移除，这里会把 flipvar 移除，因为其分数已被更新为负
+		if(score[v]<=0)
+		{
+			goodvar_stack[index] = pop(goodvar_stack);
+			already_in_goodvar_stack[v] = 0;
+		}	
+	}
+
+	//update all flipvar's neighbor's conf_change to be 1, add goodvar
+	// 唯一使用了邻居关系的地方
+	int* p;
+	for(p=var_neighbor[flipvar]; (v=*p)!=0; p++)
+	{
+		conf_change[v] = 1;
+		// 分数大于0，且还未在goodvar_stack，则入栈
+		if(score[v]>0 && already_in_goodvar_stack[v] ==0)
+		{
+			push(v,goodvar_stack);
+			already_in_goodvar_stack[v] = 1;
+		}
+	}
+}
+
+#endif
+
